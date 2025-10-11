5cebb5dd36090707b0b9b213364d6e63720dbae0
suguna-123-hub <24b11cs419@adityauniversity.in>
2025-10-11 10:44:26 +0530

    Add files via upload

---
+++ week1_5.py
@@ -0,0 +1,16 @@
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+count = 0
+
+if n <= 0:
+    print("Please enter a positive integer")
+elif n == 1:
+    print("Fibonacci sequence up to 1 term:")
+    print(a)
+else:
+    print("Fibonacci sequence:")
+    while count < n:
+        print(a, end=" ")
+        a, b = b, a + b
+        count += 1
