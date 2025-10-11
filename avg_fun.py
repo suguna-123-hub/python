5c24531fcb9e20e1156a2b39eb232bebc0ce6c4f
suguna-123-hub <24b11cs419@adityauniversity.in>
2025-10-11 10:18:23 +0530

    Add files via upload

---
+++ avg_fun.py
@@ -0,0 +1,9 @@
+def average(a,b,c,d,e):
+    return (a+b+c+d+e)/5
+num1=float(input("enter tel marks:"))
+num2=float(input("enter eng marks:"))
+num3=float(input("enter hin marks:"))
+num4=float(input("enter math marks:"))
+num5=float(input("enter sci marks:"))
+cal_avg=average(num1,num2,num3,num4,num5)
+print("average is:",cal_avg)
