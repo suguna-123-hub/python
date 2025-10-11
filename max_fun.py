d64eb637b87616628f4c098d2838e42ae5ed5dee
suguna-123-hub <24b11cs419@adityauniversity.in>
2025-10-11 10:37:19 +0530

    Add files via upload

---
+++ max_fun.py
@@ -0,0 +1,7 @@
+def maximum(a,b,c):
+    return max(a,b,c)
+num1=int(input("enter 1st num:"))
+num2=int(input("enter 2nd num:"))
+num3=int(input("enter 3rd num:"))
+ma=maximum(num1,num2,num3)
+print("maximum num is:",ma)
